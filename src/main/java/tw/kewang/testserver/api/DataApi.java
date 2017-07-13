f8da80980f1c6e1512f6369ed2fbe16aa31bb740
雲端研發部-宋慶洪 <jamesbaloney@mitake.com.tw>
2017-07-13 14:42:30 +0800

    fixed problem

--- src/main/java/tw/kewang/testserver/api/DataApi.java
+++ src/main/java/tw/kewang/testserver/api/DataApi.java
@@ -9,13 +9,13 @@ import java.util.List;
 
 @Path("data")
 public class DataApi {
-    private static volatile List<Result.Member> members = new ArrayList<>();
+    private static volatile List<Member> members = new ArrayList<>();
     private final Gson gson = new Gson();
 
     @POST
     public Response post(String body) {
         Result res = new Result();
-        Result.Member mem = gson.fromJson(body, Result.Member.class);
+        Member mem = gson.fromJson(body, Member.class);
         res.setMember(mem);
         members.add(res.getMember());
         System.out.println(gson.toJson(mem));
@@ -26,15 +26,14 @@ public class DataApi {
     @Path("{keyword}")
     @GET
     public Response get(@PathParam("keyword") String keyword) {
-        Result.Member detRes = detect(keyword);
+        Member detRes = detect(keyword);
         Result res = new Result();
-        res.setMember(detRes);
-        res.setAns("NO");
         if (detRes != null) {
+            res.setMember(detRes);
             res.setAns("OK");
-            String jsonStr = gson.toJson(res);
-            return Response.ok().entity(jsonStr).build();
+            return Response.ok().entity(gson.toJson(res)).build();
         } else {
+            res.setAns("NO");
             return Response.ok().entity(gson.toJson(res)).build();
 
         }
@@ -43,21 +42,21 @@ public class DataApi {
     @Path("{keyword}")
     @DELETE
     public Response del(@PathParam("keyword") String keyword) {
-        Result.Member detRes = detect(keyword);
+        Member detRes = detect(keyword);
         Result res = new Result();
-        res.setAns("OK");
         if (detRes != null) {
-            members.remove(detect(keyword));
+            members.remove(detRes);
         } else {
             res.setAns("NO");
         }
+        res.setAns("OK");
         return Response.ok().entity(gson.toJson(res)).build();
     }
 
     @Path("{keyword}/{dataToPut}")
     @PUT
     public Response put(@PathParam("keyword") String keyword, @PathParam("dataToPut") String dataToPut, String body) {
-        Result.Member detRes = detect(keyword);
+        Member detRes = detect(keyword);
         Result res = new Result();
         if (detRes == null) {
             res.setAns("NO");
@@ -92,17 +91,72 @@ public class DataApi {
         return Response.ok().entity(gson.toJson(res)).build();
     }
 
-    private static Result.Member detect(String key) {
+    private static Member detect(String key) {
 
         for (int numberOfIndex = 0; numberOfIndex < members.size(); numberOfIndex++) {
-            Result.Member memGet = members.get(numberOfIndex);
+            Member memGet = members.get(numberOfIndex);
             if (memGet.getPhoneNumber().equals(key) || memGet.getName().equals(key) || memGet.getEmail().equals(key)) {
                 return memGet;
             }
         }
         return null;
     }
+    private class Member {
+        private String name;
+        private String sex;
+        private String phoneNumber;
+        private String email;
+        private int age;
+        private String ans;
 
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSex() {
+            return sex;
+        }
+
+        public void setSex(String sex) {
+            this.sex = sex;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public Member(String name, String sex, int age, String phoneNumber, String email, String ans) {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+    }
     public static class Result {
         private Member member;
         private String ans;
@@ -123,62 +177,7 @@ public class DataApi {
             this.ans = ans;
         }
 
-        private class Member {
-            private String name;
-            private String sex;
-            private String phoneNumber;
-            private String email;
-            private int age;
-            private String ans;
 
-            public String getName() {
-                return name;
-            }
-
-            public void setName(String name) {
-                this.name = name;
-            }
-
-            public String getSex() {
-                return sex;
-            }
-
-            public void setSex(String sex) {
-                this.sex = sex;
-            }
-
-            public String getPhoneNumber() {
-                return phoneNumber;
-            }
-
-            public void setPhoneNumber(String phoneNumber) {
-                this.phoneNumber = phoneNumber;
-            }
-
-            public String getEmail() {
-                return email;
-            }
-
-            public void setEmail(String email) {
-                this.email = email;
-            }
-
-            public int getAge() {
-                return age;
-            }
-
-            public void setAge(int age) {
-                this.age = age;
-            }
-
-            public Member(String name, String sex, int age, String phoneNumber, String email, String ans) {
-                this.name = name;
-                this.sex = sex;
-                this.age = age;
-                this.phoneNumber = phoneNumber;
-                this.email = email;
-            }
-        }
 
 
     }
