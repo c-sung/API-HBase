df4c015dbf5c89471ddac71b1c1f33fae37b2bb2
雲端研發部-宋慶洪 <jamesbaloney@mitake.com.tw>
2017-07-13 16:03:43 +0800

    Seperated inner classes

---
+++ src/main/java/tw/kewang/testserver/api/Member.java
@@ -0,0 +1,60 @@
+package tw.kewang.testserver.api;
+
+public class Member {
+
+    private String name;
+    private String sex;
+    private String phoneNumber;
+    private String email;
+    private int age;
+    private String ans;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Member(String name, String sex, int age, String phoneNumber, String email, String ans) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
+
---
+++ src/main/java/tw/kewang/testserver/api/Result.java
@@ -0,0 +1,27 @@
+package tw.kewang.testserver.api;
+
+public class Result {
+    private Member member;
+    private String ans;
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public String getAns() {
+        return ans;
+    }
+
+    public void setAns(String ans) {
+        this.ans = ans;
+    }
+
+
+}
+
+
+
